f1be51c1060816311025a32305ddf5cfb30bab4d
Yubaraj Sarkar <yubarajsarkar2001@gmail.com>
2021-10-31 14:16:34 +0530

    updated xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,3 +1,12 @@
+--  _   _ _        _        __   __     _                      _ 
+-- | \ | (_)_ __  (_) __ _  \ \ / /   _| |__   __ _ _ __ __ _ (_)
+-- |  \| | | '_ \ | |/ _` |  \ V / | | | '_ \ / _` | '__/ _` || |    Yubaraj Sarkar (Ninja-Yubaraj)
+-- | |\  | | | | || | (_| |   | || |_| | |_) | (_| | | | (_| || |    https://github.com/Ninja-Yubaraj
+-- |_| \_|_|_| |_|/ |\__,_|___|_| \__,_|_.__/ \__,_|_|  \__,_|/ |    
+--              |__/     |_____|                            |__/ 
+-- My xmonad configuration file. This is where I started out my WM journey.
+
+
 -- IMPORTS --
 -- Base
 import XMonad							 -- Default
