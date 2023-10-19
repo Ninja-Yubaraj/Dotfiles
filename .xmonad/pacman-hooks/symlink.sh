f4d7a90566def8bc94cf76bdf309e7168802e14d
Yubaraj Sarkar <yubarajsarkar2001@gmail.com>
2023-10-19 21:45:43 +0530

    fixed symlink.sh, pywal_set_wallpaper, aftercolorz && made a new home for betterlockscreen...

--- .xmonad/pacman-hooks/symlink.sh
+++ .xmonad/pacman-hooks/symlink.sh
@@ -4,13 +4,6 @@
 # NOTE: You have to run this script as root everytime you change something in the scripts!
 
 mkdir -p /etc/pacman.d/hooks
-rm /etc/pacman.d/hooks/recompile-xmonad.hook && echo "ignore this error message if running first time"
-rm /etc/pacman.d/hooks/recompile-xmonadh.hook && echo "ignore this error message if running first time"
 
-#ln -s ~/.xmonad/pacman-hooks/recompile-xmonad.hook /etc/pacman.d/hooks/recompile-xmonad.hook
-#ln -s ~/.xmonad/pacman-hooks/recompile-xmonadh.hook /etc/pacman.d/hooks/recompile-xmonadh.hook
-#I tried to use the above two lines, but it didn't work. Error: when I tried to run the script as root, it tried to create a symlink to a file that doesn't exist in the home directory.
-
-cp recompile-xmonad.hook /etc/pacman.d/hooks/recompile-xmonad.hook
-cp recompile-xmonadh.hook /etc/pacman.d/hooks/recompile-xmonadh.hook
-# This method works too, but we need to run the script every time we change something in the scripts.
+ln -fs $PWD/recompile-xmonad.hook /etc/pacman.d/hooks/recompile-xmonad.hook
+ln -fs $PWD/recompile-xmonadh.hook /etc/pacman.d/hooks/recompile-xmonadh.hook
